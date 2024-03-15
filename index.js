47e86219c53fc1cc25223c4179d610ce6b32cb32
saraswathikasi <saraswathikasi80@gmail.com>
2024-03-15 06:34:50 +0530

    Added express to application

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Hoo");
+var express = require('express');
+const app = express();
+const port = 300;
+
+app.get('/hello', (req, res) =>{
+    res.send("Hello World! Rea")
+});
+app.get('/', (req, res) =>{
+    res.send("Hello World! Rea")
+});
+
+app.listen(port, () => {
+    console.log('Example app listening on port');
+})
+
